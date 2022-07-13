452e854fc63805fd3228eac9e79af8b7429f82fe
lightsavers <webanalyst@gmail.com>
2022-07-13 17:00:59 -0400

    contribution to unzipping subdir using python

--- 0713202_jh_unzippingZipFileRemoteURL.py
+++ 0713202_jh_unzippingZipFileRemoteURL.py
@@ -14,17 +14,20 @@ zf.namelist()
 
 dfs = []
 
-with ZipFile(BytesIO(content.content)) as z:    
-    for filename in zf.namelist():
-        if zf.is_dir():
+#s_dir = lambda zipinfo: zipinfo.filename.endswith('/')
+
+with zf as z:    
+    for filename in z.namelist():
+        file_info = z.getinfo(filename)
+        if file_info.is_dir():
             # read the file
             if filename.endswith('.csv'):
-                for line in zf.open(filename):
+                for line in z.open(filename):
                     #df = pd.read_csv(zf.open(line))
                     #print(line.decode('utf-8'))
                     
                     #zip_file = os.path.join(zf, filename)
                     #zf = zipfile.ZipFile(zip_file)
-                    dfs += [pd.read_csv(zf.open('*.csv'), header=None, sep=";", encoding='latin1') for f in zf.namelist()]
+                    dfs += [pd.read_csv(z.open(filename), header=None, sep=";", encoding='latin1') for f in z.namelist()]
 
 df = pd.concat(dfs,ignore_index=True)
